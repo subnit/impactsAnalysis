c69a6d54a33ece2630dd316f243baadad33811fa
subnit <subnit@163.com>
2020-08-30 11:42:15 +0800

    dev git util

--- src/main/java/com/subnit/util/GitMu.java
+++ src/main/java/com/subnit/util/GitMu.java
@@ -1,13 +1,5 @@
 package com.subnit.util;
 
-import com.subnit.base.git.GitUtil;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.transport.CredentialsProvider;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-
-import java.io.File;
-
 /**
  * description:
  * date : create in 上午9:56 2020/8/29
