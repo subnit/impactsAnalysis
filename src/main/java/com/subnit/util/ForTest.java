6233cd607330d9a45f6b8d3cebf4d1c0dc98e2ad
subnit <subnit@163.com>
2023-12-17 22:52:39 +0800

    dev 替换新jar包

---
+++ src/main/java/com/subnit/util/ForTest.java
@@ -0,0 +1,17 @@
+package com.subnit.util;
+
+/**
+ * description:
+ * date : create in 22:51 2023/12/17
+ * modified by :
+ *
+ * @author subo
+ */
+public class ForTest {
+    private String a;
+
+
+    public String getA() {
+        return "aa";
+    }
+}
